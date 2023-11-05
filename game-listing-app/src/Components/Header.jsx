091ec37d0a135dc0feab3cae4c1b06a14439a673
Georgiy Garenov <g_garenov@outlook.com>
2023-11-05 11:52:11 +0200

    React Hooks toggle

--- game-listing-app/src/Components/Header.jsx
+++ game-listing-app/src/Components/Header.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from './../assets/Images/logo.png'
-import {HiMagnifyingGlass, HiMoon } from "react-icons/hi2";
+import {HiMagnifyingGlass, HiMoon, HiSun } from "react-icons/hi2";
 
 function Header() {
+    const [toogle, setToggle] = useState(true);
   return (
-      <div className='flex items-center'>
+      <div className='flex items-center p-3'>
       <img src={logo} width={80} height={80} />
           <div className='flex bg-slate-500 p-2 w-full  mx-5 rounded-full items-center'>
               <HiMagnifyingGlass/>
-          <input type="text" className='bg-transparent outline-none'/>
+          <input type="text" placeholder='Search Game' className='px-2 bg-transparent outline-none'/>
           </div> 
           <div>
-              <HiMoon/>
+              {toogle
+                  ? <HiMoon className='text -[35px] bg-slate-200 text-black p-1 rounded-full' />
+                  : <HiSun className='text -[35px] bg-slate-200 text-black p-1 rounded-full' />
+              }
           </div>
       </div>
   )
