0acdfb0c7e6e4fc71c0cd718033565cea65cd049
Georgiy Garenov <g_garenov@outlook.com>
2023-11-05 11:34:55 +0200

    - React icons were installed - search icon was added

--- game-listing-app/src/Components/Header.jsx
+++ game-listing-app/src/Components/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import logo from './../assets/Images/logo.png'
+import {HiMagnifyingGlass } from "react-icons/hi2";
 
 function Header() {
   return (
-    <img src = {logo} width={80} height={80}/>
+      <div>
+      <img src={logo} width={80} height={80} />
+          <div>
+              <HiMagnifyingGlass/>
+          <input type="text" />
+          </div> 
+      </div>
   )
 }
 
