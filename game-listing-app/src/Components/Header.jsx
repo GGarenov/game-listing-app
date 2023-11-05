14788c6d9ee0d0633cc8838add1e7baac3888a77
Georgiy Garenov <g_garenov@outlook.com>
2023-11-05 11:27:25 +0200

    - Logo was imported and fixed

--- game-listing-app/src/Components/Header.jsx
+++ game-listing-app/src/Components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import logo from './../assets/Images/logo.png'
 
 function Header() {
   return (
-    <div>Header</div>
+    <img src = {logo} width={80} height={80}/>
   )
 }
 
