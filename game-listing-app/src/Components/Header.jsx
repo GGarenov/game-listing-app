1d6a0f9e876a4f24ab0a86fea837b995d3c6fda5
Georgiy Garenov <g_garenov@outlook.com>
2023-11-05 11:57:56 +0200

    on-click function was added

--- game-listing-app/src/Components/Header.jsx
+++ game-listing-app/src/Components/Header.jsx
@@ -3,7 +3,7 @@ import logo from './../assets/Images/logo.png'
 import {HiMagnifyingGlass, HiMoon, HiSun } from "react-icons/hi2";
 
 function Header() {
-    const [toogle, setToggle] = useState(true);
+    const [toggle, setToggle] = useState(true);
   return (
       <div className='flex items-center p-3'>
       <img src={logo} width={80} height={80} />
@@ -12,9 +12,11 @@ function Header() {
           <input type="text" placeholder='Search Game' className='px-2 bg-transparent outline-none'/>
           </div> 
           <div>
-              {toogle
-                  ? <HiMoon className='text -[35px] bg-slate-200 text-black p-1 rounded-full' />
-                  : <HiSun className='text -[35px] bg-slate-200 text-black p-1 rounded-full' />
+              {toggle
+                  ? <HiMoon className='text -[35px] bg-slate-200 text-black p-1 rounded-full cursor-pointer'
+                      onClick={() => setToggle(!toggle)} />
+                  : <HiSun className='text -[35px] bg-slate-200 text-black p-1 rounded-full cursor-pointer'
+                      onClick={() => setToggle(!toggle)}/>
               }
           </div>
       </div>
