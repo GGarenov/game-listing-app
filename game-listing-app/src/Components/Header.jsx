45a72345af85b0bd329f9a0e921451e1eed580ce
Georgiy Garenov <g_garenov@outlook.com>
2023-11-05 11:43:00 +0200

    Little CSS just to make it look better

--- game-listing-app/src/Components/Header.jsx
+++ game-listing-app/src/Components/Header.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import logo from './../assets/Images/logo.png'
-import {HiMagnifyingGlass } from "react-icons/hi2";
+import {HiMagnifyingGlass, HiMoon } from "react-icons/hi2";
 
 function Header() {
   return (
-      <div>
+      <div className='flex items-center'>
       <img src={logo} width={80} height={80} />
-          <div className='flex bg-slate-500 p-2 w-full items-center'>
+          <div className='flex bg-slate-500 p-2 w-full  mx-5 rounded-full items-center'>
               <HiMagnifyingGlass/>
-              
           <input type="text" className='bg-transparent outline-none'/>
           </div> 
+          <div>
+              <HiMoon/>
+          </div>
       </div>
   )
 }
