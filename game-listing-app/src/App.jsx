c23812cf556c0f4e8511f88311dce0d4150ed44b
Georgiy Garenov <g_garenov@outlook.com>
2023-11-05 11:25:02 +0200

    Header.jsx was created. Home.jsx was created logo.png was added

--- game-listing-app/src/App.jsx
+++ game-listing-app/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './Pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
-      <h2>Home Page</h2>
+      
+      <Home />
    </div>
   )
 }
